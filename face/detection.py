b5cc37c22e8b57c25c2792a58971f2e7cf035bc6
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2017-01-01 11:56:48 +0900

    Added face.detection.get_face_candidates()

--- face/detection.py
+++ face/detection.py
@@ -2,28 +2,71 @@
 Module with high level functionality for face detection
 """
 
+import shapely.geometry
+
 
 class FaceCandidate:
     """
     A simple class representing an image crop that is to be examined for face presence.
     It contains three members:
-    - image_coordinates that specify coordinates of the crop in image it was taken from
+    - crop_coordinates that specify coordinates of the crop in image it was taken from
     - cropped_image - cropped image
     - focus_coordinates - coordinates within original image for which face prediction score should of the crop
     should be used. These are generally within image_coordinates, but not necessary the same, since many partially
     overlapping crops might be examined
     """
 
-    def __init__(self, image_coordinates, cropped_image, focus_coordinates):
+    def __init__(self, crop_coordinates, cropped_image, focus_coordinates):
         """
         Constructor
-        :param image_coordinates: specify coordinates of the crop in image it was taken from
+        :param crop_coordinates: specify coordinates of the crop in image it was taken from
         :param cropped_image: cropped image
         :param focus_coordinates: coordinates within original image for which face prediction score should of the crop
         should be used. These are generally within image_coordinates, but not necessary the same, since many partially
         overlapping crops might be examined
         """
 
-        self.image_coordinates = image_coordinates
+        self.crop_coordinates = crop_coordinates
         self.cropped_image = cropped_image
         self.focus_coordinates = focus_coordinates
+
+
+def get_face_candidates(image, crop_size, step):
+    """
+    Given an image, crop size and step, get list of face candidates - crops of input image
+    that will be examined for face presence. Each crop is of crop_size and crops are taken at step distance from
+     upper left corner of one crop to next crop (thus crops might be overlapping if step is smaller than crop_size).
+     Once all possible crops have been taken scanning image in one row, scanning proceeds from first column of
+     row step away from current row.
+    :param image: image from which crops are to be taken
+    :param crop_size: size of each crop
+    :param step: step at which crops should be taken. Must be not larger than crop size.
+    :return: list of FaceCandidate objects
+    """
+
+    if crop_size < step:
+
+        raise ValueError("Crop size ({}) must be not smaller than step size ({})".format(crop_size, step))
+
+    face_candidates = []
+
+    y = 0
+
+    while y + crop_size <= image.shape[0]:
+
+        x = 0
+
+        while x + crop_size <= image.shape[1]:
+
+            crop_coordinates = shapely.geometry.box(x, y, x + crop_size, y + crop_size)
+            cropped_image = image[y:y + crop_size, x:x + crop_size]
+            focus_coordinates = shapely.geometry.box(x, y, x + step, y + step)
+
+            candidate = FaceCandidate(crop_coordinates, cropped_image, focus_coordinates)
+            face_candidates.append(candidate)
+
+            x += step
+
+        y += step
+
+    return face_candidates
---
+++ tests/face/test_detection.py
@@ -0,0 +1,119 @@
+"""
+Tests for face.detection module
+"""
+
+import numpy as np
+import shapely.geometry
+import pytest
+
+import face.detection
+
+
+def test_test_get_face_candidates_check_raises_on_step_larger_than_crop_size():
+
+    with pytest.raises(ValueError):
+
+        face.detection.get_face_candidates(np.zeros(shape=[10, 10]), crop_size=4, step=5)
+
+
+def test_get_face_candidates_single_row_crops():
+
+    image = np.arange(40).reshape([4, 10])
+    crop_size = 4
+    step = 3
+
+    face_candidates = face.detection.get_face_candidates(image, crop_size, step)
+
+    assert 3 == len(face_candidates)
+
+    # Assert properties of first candidate
+    assert 0 == np.min(face_candidates[0].cropped_image)
+    assert 33 == np.max(face_candidates[0].cropped_image)
+
+    assert shapely.geometry.box(0, 0, 4, 4) == face_candidates[0].crop_coordinates
+    assert shapely.geometry.box(0, 0, 3, 3) == face_candidates[0].focus_coordinates
+
+    # Assert properties of second candidate
+    assert 3 == np.min(face_candidates[1].cropped_image)
+    assert 36 == np.max(face_candidates[1].cropped_image)
+
+    assert shapely.geometry.box(3, 0, 7, 4) == face_candidates[1].crop_coordinates
+    assert shapely.geometry.box(3, 0, 6, 3) == face_candidates[1].focus_coordinates
+
+    # Assert properties of third candidate
+    assert 6 == np.min(face_candidates[2].cropped_image)
+    assert 39 == np.max(face_candidates[2].cropped_image)
+
+    assert shapely.geometry.box(6, 0, 10, 4) == face_candidates[2].crop_coordinates
+    assert shapely.geometry.box(6, 0, 9, 3) == face_candidates[2].focus_coordinates
+
+
+def test_get_face_candidates_single_column_crops():
+
+    image = np.arange(75).reshape([15, 5])
+    crop_size = 5
+    step = 4
+
+    face_candidates = face.detection.get_face_candidates(image, crop_size, step)
+
+    assert 3 == len(face_candidates)
+
+    # Assert properties of first candidate
+    assert 0 == np.min(face_candidates[0].cropped_image)
+    assert 24 == np.max(face_candidates[0].cropped_image)
+
+    assert shapely.geometry.box(0, 0, 5, 5) == face_candidates[0].crop_coordinates
+    assert shapely.geometry.box(0, 0, 4, 4) == face_candidates[0].focus_coordinates
+
+    # Assert properties of second candidate
+    assert 20 == np.min(face_candidates[1].cropped_image)
+    assert 44 == np.max(face_candidates[1].cropped_image)
+
+    assert shapely.geometry.box(0, 4, 5, 9) == face_candidates[1].crop_coordinates
+    assert shapely.geometry.box(0, 4, 4, 8) == face_candidates[1].focus_coordinates
+
+    # Assert properties of third candidate
+    assert 40 == np.min(face_candidates[2].cropped_image)
+    assert 64 == np.max(face_candidates[2].cropped_image)
+
+    assert shapely.geometry.box(0, 8, 5, 13) == face_candidates[2].crop_coordinates
+    assert shapely.geometry.box(0, 8, 4, 12) == face_candidates[2].focus_coordinates
+
+
+def test_get_face_candidates_simple_grid():
+
+    image = np.arange(100).reshape([10, 10])
+    crop_size = 5
+    step = 4
+
+    face_candidates = face.detection.get_face_candidates(image, crop_size, step)
+
+    assert 4 == len(face_candidates)
+
+    # Assert properties of first candidate
+    assert 0 == np.min(face_candidates[0].cropped_image)
+    assert 44 == np.max(face_candidates[0].cropped_image)
+
+    assert shapely.geometry.box(0, 0, 5, 5) == face_candidates[0].crop_coordinates
+    assert shapely.geometry.box(0, 0, 4, 4) == face_candidates[0].focus_coordinates
+
+    # Assert properties of second candidate
+    assert 4 == np.min(face_candidates[1].cropped_image)
+    assert 48 == np.max(face_candidates[1].cropped_image)
+
+    assert shapely.geometry.box(4, 0, 9, 5) == face_candidates[1].crop_coordinates
+    assert shapely.geometry.box(4, 0, 8, 4) == face_candidates[1].focus_coordinates
+
+    # Assert properties of third candidate
+    assert 40 == np.min(face_candidates[2].cropped_image)
+    assert 84 == np.max(face_candidates[2].cropped_image)
+
+    assert shapely.geometry.box(0, 4, 5, 9) == face_candidates[2].crop_coordinates
+    assert shapely.geometry.box(0, 4, 4, 8) == face_candidates[2].focus_coordinates
+
+    # Assert properties of fourth candidate
+    assert 44 == np.min(face_candidates[3].cropped_image)
+    assert 88 == np.max(face_candidates[3].cropped_image)
+
+    assert shapely.geometry.box(4, 4, 9, 9) == face_candidates[3].crop_coordinates
+    assert shapely.geometry.box(4, 4, 8, 8) == face_candidates[3].focus_coordinates
