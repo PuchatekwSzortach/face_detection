67bb6dcb5ad81386aae0e7569440349bed24c18a
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2017-01-04 23:32:43 +0900

    Added MultiScaleFaceSearchConfiguration class

--- face/config.py
+++ face/config.py
@@ -39,7 +39,47 @@ class FaceSearchConfiguration:
         self.batch_size = batch_size
 
 
+# Default face search configuration
 face_search_config = FaceSearchConfiguration(crop_size=crop_size, stride=stride, batch_size=batch_size)
+
+
+# Minimum size of a face, in pixels, we want to search for
+min_face_size = 50
+
+# Minimum ratio of face to image we want to search for. For image of size x (along smaller dimension)
+# we only want to consider regions not smaller than min_face_to_image_ratio times x as possible face candidates
+min_face_to_image_ratio = 0.015
+
+
+class MultiScaleFaceSearchConfiguration(FaceSearchConfiguration):
+    """
+    A simple class that bundles together common multi scale face search parameters
+    """
+
+    def __init__(self, crop_size, stride, batch_size, min_face_size, min_face_to_image_ratio):
+        """
+        Constructor
+        :param crop_size: size of crops used to search for faces
+        :param stride: stride between successive crops
+        :param batch_size: batch size used by predictive model
+        :param min_face_size: minimum size of a face, in pixels, we want to search for
+        :param min_face_to_image_ratio: minimum ratio of face to image we want to search for.
+        For image of size x (along smaller dimension) we only want to consider regions not smaller than
+        min_face_to_image_ratio times x as possible face candidates.
+        In practice the larger value of min_face_size and (min_face_to_image_ratio x smallest image dimension) is used
+         as smallest region used to search for faces.
+        """
+
+        super().__init__(crop_size, stride, batch_size)
+
+        self.min_face_size = min_face_size
+        self.min_face_to_image_ratio = min_face_to_image_ratio
+
+
+# Default multi scale face search configuration
+multi_scale_face_search_config = MultiScaleFaceSearchConfiguration(
+    crop_size=crop_size, stride=stride, batch_size=batch_size,
+    min_face_size=min_face_size, min_face_to_image_ratio=min_face_to_image_ratio)
 
 # Path to model file
 model_path = "../../data/faces/models/model.h5"
